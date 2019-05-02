e87e5a7d5602e38052726a589308ce2e432eebf5
adavideo <adavideo@gmail.com>
2019-05-02 18:07:16 +0200

    Simulation with 2 users

--- simulator.py
+++ simulator.py
@@ -37,16 +37,21 @@ def simulate_user_behavior(user, recommender):
 config = Config()
 simulator = GithubSimulator(config.skills, config.tasks_number * 2, 5)
 
-test_user = User("TestUser1", "", config.tasks_number)
-test_user.load_skills_from_file(config.skills)
-recommender = Recommender(test_user, simulator)
+test_user1 = User("TestUser1", "", config.tasks_number)
+test_user1.load_skills_from_file(config.skills)
+recommender1 = Recommender(test_user1, simulator)
+
+test_user2 = User("TestUser2", "", config.tasks_number)
+test_user2.load_skills_from_file(config.skills)
+recommender2 = Recommender(test_user2, simulator)
 
 print "Initial list of tasks:"
 print_tasks(simulator.tasks)
 
 iterations = int(raw_input("Select number of iterations: "))
 for i in range(1, iterations+1):
-    simulate_user_behavior(test_user, recommender)
+    simulate_user_behavior(test_user1, recommender1)
+    simulate_user_behavior(test_user2, recommender2)
 
 print "-" * 10
 print "Final list of tasks:"
