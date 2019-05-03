ace9040bb9013c21c0e121c269451cdd85ea9f1a
adavideo <adavideo@gmail.com>
2019-05-03 17:21:47 +0200

    Reordered the simulation file. Now is easier to add more users.

--- simulator.py
+++ simulator.py
@@ -5,6 +5,19 @@ from Config import Config
 from GithubSimulator import GithubSimulator
 import random
 
+def initialize_recommender(skills, tasks_number):
+    simulator = GithubSimulator(skills, tasks_number * 2, 5)
+    return Recommender(simulator)
+
+def initialize_users(number_of_users, skills, tasks_number):
+    users = []
+    for i in range(1,number_of_users+1):
+        user_name = "TestUser%d" % i
+        test_user = User(user_name, "", tasks_number)
+        test_user.load_skills_from_file(skills)
+        users.append(test_user)
+    return users
+
 def print_tasks(tasks):
     for task in tasks:
         if task.not_assigned():
@@ -32,26 +45,23 @@ def simulate_user_behavior(user):
     else:
         user.complete_current_task()
 
+def simulation(iterations):
+    for i in range(1, iterations+1):
+        for user in users:
+            simulate_user_behavior(user)
 
 config = Config()
-simulator = GithubSimulator(config.skills, config.tasks_number * 2, 5)
-recommender = Recommender(simulator)
+recommender = initialize_recommender(config.skills, config.tasks_number)
 
-users = []
-for i in range(0,2):
-    user_name = "TestUser%d" % i
-    test_user = User(user_name, "", config.tasks_number)
-    test_user.load_skills_from_file(config.skills)
-    users.append(test_user)
+number_of_users = 3
+users = initialize_users(number_of_users, config.skills, config.tasks_number)
 
 print "Initial list of tasks:"
-print_tasks(simulator.tasks)
+print_tasks(recommender.github.tasks)
 
 iterations = int(raw_input("Select number of iterations: "))
-for i in range(1, iterations+1):
-    for user in users:
-        simulate_user_behavior(user)
+simulation(iterations)
 
 print "-" * 10
 print "Final list of tasks:"
-print_tasks(simulator.tasks)
+print_tasks(recommender.github.tasks)
